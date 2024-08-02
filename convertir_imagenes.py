a94609b1e5cd54745b83e07e280254e73fb9520c
unknown <gonzalezjosejuan28@gmail.com>
2024-08-02 15:09:09 -0400

    Modificado el script run_scraper a convertir_imagenes, de modo que sea independiente el spyder y solo deba ejecutarse para convertir las imagenes del archivo correspondiente

--- convertir_imagenes.py
+++ convertir_imagenes.py
@@ -10,13 +10,12 @@ pytesseract.pytesseract.tesseract_cmd = r".\Tesseract-OCR\tesseract.exe"
 inmuebles_con_imagenes = 'inmuebles.csv'
 archivo_final = 'data.csv'
 
-star_time = time.time()
-os.system(f'scrapy crawl indomio -o {inmuebles_con_imagenes}')
-finish_time = time.time()
-print('\nTime:', finish_time - star_time)
+try:
+    time.sleep(3)
+    convertir_a_texto(inmuebles_con_imagenes, archivo_final)
+except:
+    print("No se encuentra el archivo `inmuebles.csv` o ha ocurrido un error al intentar leerlo")
 
 
-time.sleep(3)
-convertir_a_texto(inmuebles_con_imagenes, archivo_final)
 
 
